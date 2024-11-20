74b58265bdb37cbf35385f0f773949d8d64eb501
TranQuoc2k6 <qatran2k6@gmail.com>
2024-11-20 21:50:47 +0700

    Upload bài tập session02-3

---
+++ Session02-3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a = 7;
+	int b = 3;
+	int total = a+b;
+	printf("tong cua a va b la %d \n", total);
+	int signal = a-b;
+	printf("hieu cua a va b la %d \n", signal);
+	int accumulation = a*b;
+	printf("tich cua a va b la %d \n", accumulation);
+	float divide =(float) a/b;
+	printf("thuong cua a va b la %f", divide);
+	return 0;
+}
