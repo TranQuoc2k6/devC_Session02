2957052c11a64f11529bbd82d73867574a9a2bd8
TranQuoc2k6 <qatran2k6@gmail.com>
2024-11-20 22:35:27 +0700

    Upload bài tập session02-5

---
+++ Session02-5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a = 5;
+	int b = 7;
+	int perimeter = (a+b)*2;
+	printf("chu vi hinh chu nhat la %d\n",perimeter);
+	int acreage = a*b;
+	printf("dien tich hinh chu nhat la %d",acreage);
+	return 0;
+}
